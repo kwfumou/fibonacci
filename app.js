c73ccd2816a83f405cc93cf42e8d5b1ed8c01780
kwfumou <kfum2ouw9@gmail.com>
2020-05-16 20:56:46 +0900

    トリボナッチに変更した

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+memo.set(2, 1);
 function fib(n) {
-	if (memo.get(n)) return memo.get(n);
-	if (n < 1) return null;
-	if (n === 1 || n == 2) return 1;
-	memo.set(n, fib(n - 1) + fib(n - 2));
-	return memo.get(n);
+	if (memo.has(n)) return memo.get(n);
+	const result = fib(n - 1) + fib(n - 2) + fib(n - 3);
+	memo.set(n, result);
+	return result;
 }
 const length = 40;
-for (let i = 1; i <= length; i++) {
+for (let i = 0; i <= length; i++) {
 	console.log(fib(i));
 }
