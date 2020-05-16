c2f21b018bc265521f9eaa2bdf24a0d0bf8c5c62
kwfumou <kfum2ouw9@gmail.com>
2020-05-16 20:31:27 +0900

    メモ化によって高速化

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
+const memo = new Map();
 function fib(n) {
+	if (memo.get(n)) return memo.get(n);
 	if (n < 1) return null;
 	if (n === 1 || n == 2) return 1;
-	return fib(n - 1) + fib(n - 2);
+	memo.set(n, fib(n - 1) + fib(n - 2));
+	return memo.get(n);
 }
 const length = 40;
 for (let i = 1; i <= length; i++) {
