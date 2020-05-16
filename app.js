4023ab3daf930ba1a1a876dd2163b0e4d534112f
kwfumou <kfum2ouw9@gmail.com>
2020-05-16 20:02:11 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+function fib(n) {
+	if (n < 1) return null;
+	if (n === 1 || n == 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 1; i <= length; i++) {
+	console.log(fib(i));
+}
